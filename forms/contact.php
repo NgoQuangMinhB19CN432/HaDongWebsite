1238056e0f524fba3b4e4d643f961078a2f6bb81
NgoQuangMinhB19CN432 <MINHNQ.B19CN432@stu.ptit.edu.vn>
2025-07-10 21:55:12 +0700

    add smtp mail

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'ngoquangminh1509@gmail.com',
-    'password' => '',
+    'password' => 'prxy kfns dafh vnbm',
     'port' => '587'
   );
- */
+ 
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
--- forms/quote.php
+++ forms/quote.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'minhhaihd.ltd@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject = 'Request for a quote';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'ngoquangminh1509@gmail.com',
+    'password' => 'prxy kfns dafh vnbm',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
